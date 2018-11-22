fffa7725de66f3adab5e88730539a806de9b5987
Eric Shiels <ericshiels2@gmail.com>
2018-11-22 15:05:50 -0500

    Added empty controller actions

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,4 +1,24 @@
 class AppointmentsController < ApplicationController
+
   def index
   end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+  
 end
--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,2 +1,6 @@
 class StaticController < ApplicationController
+
+  def index
+
+  end
 end
