13e2fde399d5b91c9d7cd2faaa6999c7a753fe5f
Eric Shiels <ericshiels2@gmail.com>
2018-11-22 11:00:01 -0500

    Adding nested resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,17 @@
 Rails.application.routes.draw do
-  resources :appointments
-  resources :patients
-  resources :doctors
+  resources :appointments do
+    resources :patients
+    resources :doctors
+  end
+
+  resources :patients do
+    resources :appointments
+  end
+
+  resources :doctors do
+    resources :appointments
+  end
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
