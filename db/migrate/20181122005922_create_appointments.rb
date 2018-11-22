70c0b62447eea2bc0bef7f78749fe4798c746bdc
Eric Shiels <ericshiels2@gmail.com>
2018-11-22 13:44:34 -0500

    adding foeign keys to appointment and migrating

--- db/migrate/20181122005922_create_appointments.rb
+++ db/migrate/20181122005922_create_appointments.rb
@@ -1,6 +1,8 @@
 class CreateAppointments < ActiveRecord::Migration[5.1]
   def change
     create_table :appointments do |t|
+      t.belongs_to :doctor
+      t.belongs_to :patient
       t.string :date
       t.text :description
 
