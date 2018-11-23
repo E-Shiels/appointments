568c4d0f10b24c8b80bd084f6785a0a71882f683
Eric Shiels <ericshiels2@gmail.com>
2018-11-23 14:41:15 -0500

    Working on appointment show view

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -2,9 +2,11 @@ class AppointmentsController < ApplicationController
 
   def index
     @appointments = Appointment.all
+    #@appointments = Appointment.where("patient_id = ?", params[:user_id])
   end
 
   def show
+    @appointment = Appointment.find(params[:id])
   end
 
   def new
