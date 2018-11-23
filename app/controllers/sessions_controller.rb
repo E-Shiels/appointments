a347aab90c4042d8a9087cdfd7b05fea955783c7
Eric Shiels <ericshiels2@gmail.com>
2018-11-23 14:07:23 -0500

    Working on login form

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,18 +1,23 @@
 class SessionsController < ApplicationController
-
   def new
   end
 
-  # def create
-  #   if params[:email].present? && params[:password].present?
-  #     user = Doctor.find_by(:email => params[:email]) || Patient.find_by(:email => params[:email])
-  #     if user && user.authenticate(params[:password])
-  #       login(user)
-  #       if user.is_a?(Appointment)
-  #         redirect_to
-  #       end
-  #     end
-  # end
+  def create
+    if params[:session][:email].present? && params[:session][:password].present?
+      user = Doctor.find_by(:email => params[:session][:email]) || Patient.find_by(:email => params[:session][:email])
+      if user && user.authenticate(params[:session][:password])
+        login(user)
+        if user.is_a?(Doctor)
+          redirect_to doctor_path(user)
+        elsif user.is_a?(Patient)
+          redirect_to patient_path(user)
+        end
+      else
+        flash.now[:notice] = "Error. Please, try again."
+        render :new
+      end
+    end
+  end
 
   def destroy
     reset_session
