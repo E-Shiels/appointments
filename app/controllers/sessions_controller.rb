f34386aeb73177c2fac712ed28f6910717bd981b
Eric Shiels <ericshiels2@gmail.com>
2018-11-23 10:31:41 -0500

    Working on sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,16 @@ class SessionsController < ApplicationController
   def new
   end
 
-  def create
-  end
+  # def create
+  #   if params[:email].present? && params[:password].present?
+  #     user = Doctor.find_by(:email => params[:email]) || Patient.find_by(:email => params[:email])
+  #     if user && user.authenticate(params[:password])
+  #       login(user)
+  #       if user.is_a?(Appointment)
+  #         redirect_to
+  #       end
+  #     end
+  # end
 
   def destroy
     reset_session
