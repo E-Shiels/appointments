384aacaa00e1d9803857dbdbfd000e3678eb5213
Eric Shiels <ericshiels2@gmail.com>
2018-11-26 09:44:06 -0500

    Almost finished doctors controller

--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -2,31 +2,55 @@ class DoctorsController < ApplicationController
 
   def index
     @doctors = Doctor.all
-    #TEMPORARY
   end
 
   def show
-    @doctor = Doctor.find(params[:id])
+    find_doctor_from_params
   end
 
   def new
+    @doctor = Doctor.new
   end
 
   def create
+    @doctor = Doctor.new(doctor_params)
+    if @doctor.save
+      session[:user_id] = @doctor.id
+      flash[:notice] = "You have successfully signed up."
+      redirect_to @doctor
+    else
+      flash[:notice] = "Signup failed"
+      render :new
+    end
   end
 
   def edit
+    find_doctor_from_params
   end
 
   def update
+    find_doctor_from_params
+    if @doctor.update(doctor_params)
+      flash[:notice] = "Doctor was successfully updated."
+      redirect_to :root
+    else
+      flash[:notice] = "Doctor update failed."
+      render :new
+    end
   end
 
   def destroy
+    find_doctor_from_params
+    @doctor.destroy
   end
 
   private
   def doctor_params
     params.require(:doctor).permit(:name, :gender, :specialty, :practice_name, :practice_location, :phone, :email, :password)
+  end
+
+  def find_doctor_from_params
+    @doctor = Doctor.find(params[:id])
   end
 
 end
