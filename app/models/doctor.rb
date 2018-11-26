77d56edcb7251f20e731ac7d9473ce2e916d6bfe
Eric Shiels <ericshiels2@gmail.com>
2018-11-26 18:03:05 -0500

    Adding extra validations to doctors

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -6,6 +6,9 @@ class Doctor < ApplicationRecord
 
   validates :name, presence: true
   validates :gender, presence: true
+  validates :age, presence: true,
+  validates :age, numericality: true,
+  validates :address, presence: true
 #  validates :attribute, phone: true
   validates :email, 'valid_email_2/email': { mx: true, disposable: true}
 end
