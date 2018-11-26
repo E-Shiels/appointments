d69e63009a7376f9048194694d85cfc5cc3b5840
Eric Shiels <ericshiels2@gmail.com>
2018-11-26 16:25:10 -0500

    Working on sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,21 +1,30 @@
 class SessionsController < ApplicationController
   def new
+    if logged_in?
+      doctor = Doctor.find_by_id(session[:doctor_id])
+      patient = Patient.find_by_id(session[:patient_id])
+      if doctor
+        redirect_to doctor_path(doctor)
+      elsif patient
+        redirect_to patient_path(patient)
+      end
+    end
   end
 
   def create
     if params[:session][:email].present? && params[:session][:password].present?
-      user = Doctor.find_by(:email => params[:session][:email]) || Patient.find_by(:email => params[:session][:email])
-      if user && user.authenticate(params[:session][:password])
-        login(user)
-        if user.is_a?(Doctor)
-          redirect_to doctor_path(user)
-        elsif user.is_a?(Patient)
-          redirect_to patient_path(user)
-        end
+      doctor = Doctor.find_by(:email => params[:session][:email])
+      patient = Patient.find_by(:email => params[:session][:email])
+      if doctor && doctor.authenticate(params[:session][:password])
+        login_doctor(doctor)
+        redirect_to doctor_path(doctor)
+      elsif patient && patient.authenticate(params[:session][:password])
+        login_patient(patient)
+          redirect_to patient_path(patient)
+      end
       else
         flash.now[:notice] = "Error. Please, try again."
         render :new
-      end
     end
   end
 
