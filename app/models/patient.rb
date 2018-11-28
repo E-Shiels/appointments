d47003598acfb0c26ffdb0a89dec01c04d201e32
Eric Shiels <ericshiels2@gmail.com>
2018-11-27 21:41:34 -0500

    Adding simple create_with_omniauth method

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -11,4 +11,13 @@ class Patient < ApplicationRecord
 #  validates :attribute, phone: true
   validates :email, 'valid_email_2/email': { mx: true, disposable: true}
   validates :email, uniqueness: true
+
+  def self.create_with_omniauth(auth)
+    create! do |user|
+      user.provider = auth["provider"]
+      user.uid = auth["uid"]
+      user.name = auth["info"]["name"]
+    end
+  end
+  
 end
