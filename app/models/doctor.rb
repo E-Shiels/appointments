8f73d5deffc4fda2f0abdd6dd965f10a0af79dfb
Eric Shiels <ericshiels2@gmail.com>
2018-11-30 13:57:35 -0500

    Fixing doctor validations

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -6,9 +6,9 @@ class Doctor < ApplicationRecord
 
   validates :name, presence: true
   validates :gender, presence: true
-  validates :age, presence: true
-  validates :age, numericality: true
-  validates :address, presence: true
+  validates :specialty, presence: true
+  validates :practice_name, presence: true
+  validates :practice_location, presence: true
 #  validates :attribute, phone: true
   validates :email, 'valid_email_2/email': { mx: true, disposable: true}
   validates :email, uniqueness: true
