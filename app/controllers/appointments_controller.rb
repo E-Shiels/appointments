5823de3cfc2b8a358a63c43c135f208f660970f3
Eric Shiels <ericshiels2@gmail.com>
2018-12-06 21:02:53 -0500

    DRY appointments controller

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,154 +1,122 @@
 class AppointmentsController < ApplicationController
+  before_action :login_required
+  before_action :find_appointment_from_params, except: [:index, :new, :create]
+
 
   def index
-    if logged_in?
-      if current_patient
-        @appointments = Appointment.where("patient_id = ?", current_patient.id)
-      elsif current_doctor
-        @appointments = Appointment.where("doctor_id = ?", current_doctor.id)
-      end
+      if @current_patient
+        @appointments = Appointment.where("patient_id = ?", @current_patient.id)
+      elsif @current_doctor
+        @appointments = Appointment.where("doctor_id = ?", @current_doctor.id)
     else
       flash[:alert] = "You can't view appointments becuase you aren't logged in."
-      redirect_to :root
+      redirect_to root_path
     end
   end
 
   def show
-    find_appointment_from_params
-    if logged_in?
-      if current_patient
-        unless current_patient = @appointment.patient
+      if @current_patient
+        unless @current_patient = @appointment.patient
           flash[:alert] = "You can't view this appointment."
-          redirect_to :appointments
+          redirect_to appointments_path
         end
-      elsif current_doctor
-        unless current_doctor = @appointment.doctor
+      elsif @current_doctor
+        unless @current_doctor = @appointment.doctor
           flash[:alert] = "You can't view this appointment."
-          redirect_to :appointments
+          redirect_to appointments_path
         end
+      else
+        flash[:alert] = "You can't view appointments becuase you aren't logged in."
+        redirect_to root_path
       end
-    else
-      flash[:alert] = "You can't view appointments becuase you aren't logged in."
-      redirect_to :root
     end
   end
 
   def new
     @appointment = Appointment.new
-    unless logged_in? && current_doctor
+    unless logged_in? && @current_doctor
       flash[:alert] = "You must be signed in as a doctor to create an appointment."
       if !logged_in?
-      redirect_to :root
+      redirect_to root_path
       else
-        redirect_to patient_path(current_patient)
+        redirect_to patient_path(@current_patient)
       end
     end
   end
 
   def create
     @appointment = Appointment.new(appointment_params)
-    if logged_in?
-      if current_doctor
+      if @current_doctor
         if @appointment.save
           @appointment.date = Date.parse(params[:appointment][:date]) unless Date.parse(params[:appointment][:date]).nil?
           @appointment.save
           flash[:notice] = "Appointment sucessfully created"
-          redirect_to @appointment
+          redirect_to appointment_path(@appointment)
         else
-          flash[:alert] = "Appointment creation failed"
+          flash.now[:alert] = "Appointment creation failed"
           render :new
         end
       else
         flash[:alert] = "You can't create an appointment."
-        redirect_to patient_path(current_patient)
+        redirect_to patient_path(@current_patient)
       end
-    else
-      flash[:alert] = "You need to log in to create an appointment."
-      redirect_to :root
     end
-  end
 
   def edit
-    find_appointment_from_params
-    if logged_in?
-      if current_doctor
-        unless current_doctor = @appointment.doctor
+      if @current_doctor
+        unless c@urrent_doctor = @appointment.doctor
           flash[:alert] = "You can't edit this appointment."
-          redirect_to :appointments
+          redirect_to appointments_path(@appointment)
         end
-      elsif current_patient
-        unless curent_patient = @appointment.patient
+      elsif @current_patient
+        unless @curent_patient = @appointment.patient
           flash[:alert] = "You can't edit thiss appointment."
-          redirect_to :appointments
+          redirect_to appointments_path
         end
       end
-    else
-      flash[:alert] = "You must be signed in to edit an appointment."
-      redirect_to :root
-    end
   end
 
   def update
-    find_appointment_from_params
-    if logged_in?
-      if current_patient
-        if current_patient = @appointment.patient
+      if @current_patient
+        if @current_patient = @appointment.patient
           if @appointment.update(appointment_params)
             flash[:notice] = "Appointment sucessfully updated"
             redirect_to appointment_path(@appointment)
           else
-            flash[:alert] = "Appointment update failed"
+            flash.now[:alert] = "Appointment update failed"
             render :edit
           end
         else
           flash[:alert] = "You can't edit this appointment."
-          redirect_to :appointments
+          redirect_to appointments_path
         end
-      elsif current_doctor
-        if current_doctor = @appointment.doctor
+      elsif @current_doctor
+        if @current_doctor = @appointment.doctor
           if @appointment.update(appointment_params)
             flash[:notice] = "Appointment sucessfully updated"
             redirect_to appointment_path(@appointment)
           else
-            flash[:alert] = "Appointment update failed"
+            flash.now[:alert] = "Appointment update failed"
             render :edit
           end
         else
           flash[:alert] = "You can't edit this appointment."
-          redirect_to :appointments
+          redirect_to appointments_path
         end
-      else
-        redirect_to :root
-      end
     end
-end
 
   def destroy
-    find_appointment_from_params
-    if logged_in?
-      if current_doctor
-        if current_doctor = @appointment.doctor
+      if @current_doctor
+        if @current_doctor = @appointment.doctor
           @appointment.destroy
           flash[:notice] = "Appointment sucessfully cancelled"
-          redirect_to :appointments
+          redirect_to appointments_path
         else
           flash[:alert] = "You can't cancel this appointment."
-          redirect_to :appointments
-        end
-      elsif current_patient
-        if current_patient = @appintment.patient
-          @appointment.destroy
-          flash[:alert] = "Appointment sucessfully cancelled"
-          redirect_to :appointments
-        else
-          flash[:alert] = "You can't cancel this appointment."
-          redirect_to :appointments
+          redirect_to appointments_path
         end
       end
-    else
-      redirect_to :root
     end
-  end
 
   private
   def appointment_params
