53afbf3e53d4f5f55d08f0e57cc9e6669b2cbbc7
Eric Shiels <ericshiels2@gmail.com>
2018-12-06 20:23:55 -0500

    DRY patients controller

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -1,73 +1,71 @@
 class PatientsController < ApplicationController
+  before_action :login_required, except: [:new, :create]
+  before_action :find_patient_from_params, except: [:new, :create]
 
   def show
-    find_patient_from_params
-    if logged_in?
-      if current_patient = @patient
-        @appointments = Appointment.this_week.where("patient_id = ?", current_patient.id)
-      else
-      doctor = Doctor.find_by_id(session[:doctor_id])
-      patient = Patient.find_by_id(session[:patient_id])
-      if doctor
-        redirect_to doctor_path(doctor)
-      elsif patient
-        redirect_to patient_path(patient)
-      else
-        redirect_to :root
-      end
-    end
+    if @current_patient = @patient
+      @appointments = Appointment.this_week.where("patient_id = ?", current_patient.id)
     else
-    redirect_to :root
+      if @current_doctor
+        redirect_to doctor_path(@current_doctor)
+      elsif @current_patient
+        redirect_to patient_path(@current_patient)
+      else
+        redirect_to root_path
+      end
     end
   end
 
   def new
-    if logged_in?
-      doctor = Doctor.find_by_id(session[:doctor_id])
-      patient = Patient.find_by_id(session[:patient_id])
-      if doctor
-        redirect_to doctor_path(doctor)
-      elsif patient
-        redirect_to patient_path(patient)
+      if @current_doctor
+        redirect_to doctor_path(@current_doctor)
+      elsif @current_patient
+        redirect_to patient_path(@current_patient)
       end
-    end
     @patient = Patient.new
   end
 
   def create
     @patient = Patient.new(patient_params)
+
+    if @current_patient
+      flash[:alert] = "You already have an account."
+      redirect_to patient_path(@current_patient)
+    elsif @current_doctor
+      flash[:alert] = "You can't create a patient account."
+      redirect_to patient_path(@patient)
+    end
+
     if @patient.save
-      session[:user_id] = @patient.id
+      login_patient(@patient)
       flash[:notice] = "You have successfully signed up."
       redirect_to patient_path(@patient)
     else
-      flash[:alert] = "Signup failed"
+      flash.now[:alert] = "Signup failed. Doctor account couldn't be created."
       render :new
     end
   end
 
   def edit
-    find_patient_from_params
-    if current_patient
+    if @current_patient
       unless current_patient == @patient
-        redirect_to patient_path(current_patient)
+        redirect_to patient_path(@current_patient)
       end
-    elsif current_doctor
-      redirect_to doctor_path(current_doctor)
+    elsif @current_doctor
+      redirect_to doctor_path(@current_doctor)
     else
-      redirect_to :root
+      redirect_to root_path
     end
   end
 
   def update
-    find_patient_from_params
-    if current_patient
-      if current_patient == @patient
+    if @current_patient
+      if @current_patient == @patient
         if @patient.update(patient_params)
           flash[:notice] = "Patient details were successfully updated."
-          redirect_to :root
+          redirect_to patient_path(@patient)
         else
-          flash[:alert] = "Patient update failed."
+          flash[:alert] = "Patient update failed. Fix the fields indicated by the error messages and try again."
           render :new
         end
       else
@@ -79,9 +77,8 @@ class PatientsController < ApplicationController
   end
 
   def destroy
-    find_patient_from_params
-    if current_patient
-      if current_patient == @patient
+    if @current_patient
+      if @current_patient == @patient
         @patient.destroy
         reset_session
         flash[:notice] = "You have successfully deleted your account."
