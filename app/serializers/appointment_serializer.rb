b5a2f8903a522f5e389039352cad7186dc1f6b14
Eric Shiels <ericshiels2@gmail.com>
2019-02-10 20:07:08 -0500

    Finished appointment serializer for step 1

--- app/serializers/appointment_serializer.rb
+++ app/serializers/appointment_serializer.rb
@@ -1,5 +1,5 @@
 class AppointmentSerializer < ActiveModel::Serializer
   attributes :id, :date, :time, :description
-  belongs_to :doctor
-  belongs_to :patient
+  belongs_to :doctor, serializer: AppointmentDoctorSerializer
+  belongs_to :patient, serializer: AppointmentPatientSerializer
 end
