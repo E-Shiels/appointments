362979555209b21f755b3a66dd9cd556dadb280e
Eric Shiels <ericshiels2@gmail.com>
2019-02-12 11:02:04 -0500

    Appointment index now works with 1 appointment

--- app/assets/javascripts/appointments.js
+++ app/assets/javascripts/appointments.js
@@ -1,10 +1,14 @@
 class Appointment {
-  constructor(date, time, description, doctor_id, patient_id) {
+  constructor(date, time, description, doctor_id, patient_id, doctor_name, patient_name, doctor_slug, patient_slug) {
     this.date = date;
     this.time = time;
     this.description = description;
     this.doctor_id = doctor_id;
     this.patient_id = patient_id;
+    this.doctor_name = doctor_name;
+    this.patient_name = patient_name;
+    this.doctor_slug = doctor_slug;
+    this.patient_slug = patient_slug;
   }
 }
 
@@ -13,19 +17,24 @@ $(function() {
     let i;
     let appointments = [];
 
-    for (i = 0; i < data.today.length; i++) {
-      let a = new Appointment(data.today[i].date,
-        data.today[i].time,
-        data.today[i].description,
-        data.today[i].doctor_id,
-        data.today[i].patient_id);
+    for (i = 0; i < data.length; i++) {
+      let a = new Appointment(data[i].date,
+        data[i].time,
+        data[i].description,
+        data[i].doctor_id,
+        data[i].patient_id,
+        data[i].doctor.name,
+        data[i].patient.name,
+        data[i].doctor.slug,
+        data[i].patient.slug);
       appointments.push(a);
     }
 
     appointments.forEach(function(a) {
       $('.doctor-appointment-date').html(`${a.date}`);
       $('.doctor-appointment-time').html(`${a.time}`);
-        debugger
+      $('.doctor-appointment-patient').html(`<a href='/patients/${a.patient_slug}'>${a.patient_name}</a>`);
+      $('.doctor-appointment-description').html(`${a.description}`);
     });
   });
 });
