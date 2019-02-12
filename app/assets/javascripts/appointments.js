aab4ca5f0fa74ce7b62b0d7efa85e14ba09edd70
Eric Shiels <ericshiels2@gmail.com>
2019-02-12 12:23:23 -0500

    Replaces Ruby view with JS and jQuery

--- app/assets/javascripts/appointments.js
+++ app/assets/javascripts/appointments.js
@@ -1,5 +1,6 @@
 class Appointment {
-  constructor(date, time, description, doctor_id, patient_id, doctor_name, patient_name, doctor_slug, patient_slug) {
+  constructor(r_id, date, time, description, doctor_id, patient_id, doctor_name, patient_name, doctor_slug, patient_slug) {
+    this.r_id = r_id;
     this.date = date;
     this.time = time;
     this.description = description;
@@ -18,7 +19,9 @@ $(function() {
     let appointments = [];
 
     for (i = 0; i < data.length; i++) {
-      let a = new Appointment(data[i].date,
+      let a = new Appointment(
+        data[i].id,
+        data[i].date,
         data[i].time,
         data[i].description,
         data[i].doctor_id,
@@ -29,12 +32,18 @@ $(function() {
         data[i].patient.slug);
       appointments.push(a);
     }
-
+    let o = 0;
     appointments.forEach(function(a) {
-      $('.doctor-appointment-date').html(`${a.date}`);
-      $('.doctor-appointment-time').html(`${a.time}`);
-      $('.doctor-appointment-patient').html(`<a href='/patients/${a.patient_slug}'>${a.patient_name}</a>`);
-      $('.doctor-appointment-description').html(`${a.description}`);
+      $('#doctor-appointments-section').append(`<div id='div-${o}'></div>`)
+      $(`#div-${o}`).append(`<table id='table-${o}'></table>`)
+      $(`#table-${o}`).append(`<tr><th>Date</th><td>${a.date}</td></tr>`);
+      $(`#table-${o}`).append(`<tr><th>Time</th><td>${a.time}</td></tr>`);
+      $(`#table-${o}`).append(`<tr><th>Patient</th><td><a href='/patients/${a.patient_slug}'>${a.patient_name}</a></td></tr>`);
+      $(`#table-${o}`).append(`<tr><th>Description</th><td>${a.description}</td></tr>`);
+      $(`#div-${o}`).append(`<a href='/appointments/${a.r_id}' class='show-page-button'>View Full Details</a>`);
+      $(`#div-${o}`).append('<hr>');
+      o++;
+      //debugger
     });
   });
 });
