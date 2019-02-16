8d3e100f37a336d982b8210f7df1527384b6b6be
Eric Shiels <ericshiels2@gmail.com>
2019-02-16 18:10:18 -0500

    Fix pluralization of doctor in has_many

--- app/serializers/patient_serializer.rb
+++ app/serializers/patient_serializer.rb
@@ -1,4 +1,4 @@
 class PatientSerializer < ActiveModel::Serializer
   attributes :id, :name, :gender, :age, :address, :phone, :email, :slug
-  has_many :doctor, serializer: PatientDoctorSerializer
+  has_many :doctors, serializer: PatientDoctorSerializer
 end
