e936f111811f5e15a18d415b7037c283e4a88a6f
Eric Shiels <ericshiels2@gmail.com>
2019-02-18 16:30:14 -0500

    Linting and Beautifying

--- app/assets/javascripts/appointments.js
+++ app/assets/javascripts/appointments.js
@@ -1,52 +1,52 @@
 class Appointment {
-  constructor(r_id, date, time, description, doctor_id, patient_id, doctor_name, patient_name, doctor_slug, patient_slug) {
-    this.r_id = r_id;
-    this.date = date;
-    this.time = time;
-    this.description = description;
-    this.doctor_id = doctor_id;
-    this.patient_id = patient_id;
-    // Considered instantiating classes for these objects, but I feel that this is easier for now
-    this.doctor_name = doctor_name;
-    this.patient_name = patient_name;
-    this.doctor_slug = doctor_slug;
-    this.patient_slug = patient_slug;
-  }
-  patient_url() {
-    return `/patients/${this.patient_slug}`;
-  }
+    constructor(r_id, date, time, description, doctor_id, patient_id, doctor_name, patient_name, doctor_slug, patient_slug) {
+        this.r_id = r_id;
+        this.date = date;
+        this.time = time;
+        this.description = description;
+        this.doctor_id = doctor_id;
+        this.patient_id = patient_id;
+        // Considered instantiating classes for these objects, but I feel that this is easier for now
+        this.doctor_name = doctor_name;
+        this.patient_name = patient_name;
+        this.doctor_slug = doctor_slug;
+        this.patient_slug = patient_slug;
+    }
+    patient_url() {
+        return `/patients/${this.patient_slug}`;
+    }
 }
 
 $(function() {
-  $.getJSON('/appointments.json').success(function(data) {
-    let i;
-    let appointments = [];
+    $.getJSON('/appointments.json').success(function(data) {
+        let i;
+        let appointments = [];
 
-    for (i = 0; i < data.length; i++) {
-      let a = new Appointment(
-        data[i].id,
-        data[i].date,
-        data[i].time,
-        data[i].description,
-        data[i].doctor_id,
-        data[i].patient_id,
-        data[i].doctor.name,
-        data[i].patient.name,
-        data[i].doctor.slug,
-        data[i].patient.slug);
-      appointments.push(a);
-    }
-    let o = 0;
-    appointments.forEach(function(a) {
-      $('#doctor-appointments-section').append(`<div id='div-${o}'></div>`);
-      $(`#div-${o}`).append(`<table id='table-${o}'></table>`);
-      $(`#table-${o}`).append(`<tr><th>Date</th><td>${a.date}</td></tr>`);
-      $(`#table-${o}`).append(`<tr><th>Time</th><td>${a.time}</td></tr>`);
-      $(`#table-${o}`).append(`<tr><th>Patient</th><td><a href='${a.patient_url()}'>${a.patient_name}</a></td></tr>`);
-      $(`#table-${o}`).append(`<tr><th>Description</th><td>${a.description}</td></tr>`);
-      $(`#div-${o}`).append(`<a href='/appointments/${a.r_id}' class='show-page-button'>View Full Details</a>`);
-      $(`#div-${o}`).append('<hr>');
-      o++;
+        for (i = 0; i < data.length; i++) {
+            let a = new Appointment(
+                data[i].id,
+                data[i].date,
+                data[i].time,
+                data[i].description,
+                data[i].doctor_id,
+                data[i].patient_id,
+                data[i].doctor.name,
+                data[i].patient.name,
+                data[i].doctor.slug,
+                data[i].patient.slug);
+            appointments.push(a);
+        }
+        let o = 0;
+        appointments.forEach(function(a) {
+            $('#doctor-appointments-section').append(`<div id='div-${o}'></div>`);
+            $(`#div-${o}`).append(`<table id='table-${o}'></table>`);
+            $(`#table-${o}`).append(`<tr><th>Date</th><td>${a.date}</td></tr>`);
+            $(`#table-${o}`).append(`<tr><th>Time</th><td>${a.time}</td></tr>`);
+            $(`#table-${o}`).append(`<tr><th>Patient</th><td><a href='${a.patient_url()}'>${a.patient_name}</a></td></tr>`);
+            $(`#table-${o}`).append(`<tr><th>Description</th><td>${a.description}</td></tr>`);
+            $(`#div-${o}`).append(`<a href='/appointments/${a.r_id}' class='show-page-button'>View Full Details</a>`);
+            $(`#div-${o}`).append('<hr>');
+            o++;
+        });
     });
-  });
 });
--- app/assets/javascripts/patients.js
+++ app/assets/javascripts/patients.js
@@ -1,51 +1,53 @@
 class Patient {
-  constructor(r_id, name, gender, age, address, phone, email, slug) {
-    this.r_id = r_id;
-    this.name = name;
-    this.gender = gender;
-    this.age = age;
-    this.address = address;
-    this.phone = phone;
-    this.email = email;
-    this.slug = slug;
-  }
+    constructor(r_id, name, gender, age, address, phone, email, slug) {
+        this.r_id = r_id;
+        this.name = name;
+        this.gender = gender;
+        this.age = age;
+        this.address = address;
+        this.phone = phone;
+        this.email = email;
+        this.slug = slug;
+    }
 }
 
 $(function() {
-  let path = window.location.pathname;
-  let doctorsList = [];
+    let path = window.location.pathname;
+    let doctorsList = [];
 
-  $.getJSON(path).success(function(data) {
-    let doctorIds = [];
-    for (let i = 0; i < data.doctors.length; i++) {
-      let newDoctor = true;
-      for(let j = 0; j < doctorIds.length; j++) {
-        if( doctorIds[j] === data.doctors[i].id){
-          newDoctor = false;
-          break;
+    $.getJSON(path).success(function(data) {
+        let doctorIds = [];
+        for (let i = 0; i < data.doctors.length; i++) {
+            let newDoctor = true;
+            for (let j = 0; j < doctorIds.length; j++) {
+                if (doctorIds[j] === data.doctors[i].id) {
+                    newDoctor = false;
+                    break;
+                }
+            }
+            if (newDoctor) {
+                doctorIds.push(data.doctors[i].id);
+                let doctor = data.doctors[i];
+                let d = new Doctor(
+                    doctor.id,
+                    doctor.name,
+                    "",
+                    doctor.specialty,
+                    "",
+                    "",
+                    "",
+                    "",
+                    doctor.slug);
+                doctorsList.push(d);
+            }
         }
-      }
-      if( newDoctor ) {
-        doctorIds.push(data.doctors[i].id);
-        let doctor = data.doctors[i];
-        let d = new Doctor(
-          doctor.id,
-          doctor.name,
-          "",
-          doctor.specialty,
-          "",
-          "",
-          "",
-          "",
-          doctor.slug);
-          doctorsList.push(d);
-        }
-      }
 
     }).then(function() {
-      doctorsList.forEach(function(doctor) {
-        $('#patient-doctors-section').append(`<table id="patient-doctors-list"><th><td>${doctor.name}</td></th></table>`);
-      });
-    }
-  );
+        doctorsList.forEach(function(doctor) {
+            $('#patient-doctors-section').append(`<table id="patient-doctors-list" data-id="${doctor.r_id}"></table>`);
+            $(`#patient-doctors-list[data-id='${doctor.r_id}']`).append(`<tr><th>Name</th><td><a href="/doctors/${doctor.slug}">${doctor.name}</a></td></tr>`);
+            $(`#patient-doctors-list[data-id='${doctor.r_id}']`).append(`<tr><th>Specialty</th><td>${doctor.specialty}</td></tr>`);
+            $(`#patient-doctors-list[data-id='${doctor.r_id}']`).after(`<hr>`);
+        });
+    });
 });
