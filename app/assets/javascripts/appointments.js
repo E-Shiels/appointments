b80a5f50239ac6706ba0067bc28ae12e8c3ba5fd
Eric Shiels <ericshiels2@gmail.com>
2019-02-25 14:43:23 -0500

    Fixes property and method names to be in JS format

--- app/assets/javascripts/appointments.js
+++ app/assets/javascripts/appointments.js
@@ -1,18 +1,18 @@
 class Appointment {
-  constructor(id, date, time, description, doctor_id, patient_id, doctor_name, patient_name, doctor_slug, patient_slug) {
+  constructor(id, date, time, description, doctorId, patientId, doctorName, patientName, doctorSlug, patientSlug) {
     this.id = id;
     this.date = date;
     this.time = time;
     this.description = description;
-    this.doctor_id = doctor_id;
-    this.patient_id = patient_id;
-    this.doctor_name = doctor_name;
-    this.patient_name = patient_name;
-    this.doctor_slug = doctor_slug;
-    this.patient_slug = patient_slug;
+    this.doctorId = doctorId;
+    this.patientId = patientId;
+    this.doctorName = doctorName;
+    this.patientName = patientName;
+    this.doctorSlug = doctorSlug;
+    this.patientSlug = patientSlug;
   }
-  patient_url() {
-    return `/patients/${this.patient_slug}`;
+  patientUrl() {
+    return `/patients/${this.patientSlug}`;
   }
 }
 
@@ -25,15 +25,14 @@ function getAndAppendAppointments() {
   $.getJSON('/appointments.json').success(function(data) {
     let i;
     let appointments = [];
-
     for (i = 0; i < data.length; i++) {
       let a = new Appointment(
         data[i].id,
         data[i].date,
         data[i].time,
         data[i].description,
-        data[i].doctor_id,
-        data[i].patient_id,
+        data[i].doctor.id,
+        data[i].patient.id,
         data[i].doctor.name,
         data[i].patient.name,
         data[i].doctor.slug,
@@ -47,7 +46,7 @@ function getAndAppendAppointments() {
       $(`#div-${o}`).append(`<table id='table-${o}'></table>`);
       $(`#table-${o}`).append(`<tr><th>Date</th><td>${a.date}</td></tr>`);
       $(`#table-${o}`).append(`<tr><th>Time</th><td>${a.time}</td></tr>`);
-      $(`#table-${o}`).append(`<tr><th>Patient</th><td><a href='${a.patient_url()}'>${a.patient_name}</a></td></tr>`);
+      $(`#table-${o}`).append(`<tr><th>Patient</th><td><a href='${a.patientUrl()}'>${a.patientName}</a></td></tr>`);
       $(`#div-${o}`).append(`<a href='/appointments/${a.id}' class='show-page-button'>View Full Details</a>`);
       $(`#div-${o}`).append('<hr>');
       o++;
