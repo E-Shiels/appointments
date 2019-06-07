a667f26726c295030432de44390034472738d585
Eric Shiels <ericshiels2@gmail.com>
2019-06-07 13:55:42 -0400

    Update bypass_ssl_verification.rb

--- config/initializers/bypass_ssl_verification.rb
+++ config/initializers/bypass_ssl_verification.rb
@@ -1,5 +1,5 @@
  require 'openssl'
-# #OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
+ OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
 #
- OpenSSL::SSL.send(:remove_const, :VERIFY_PEER)
- OpenSSL::SSL.const_set(:VERIFY_PEER, OpenSSL::SSL::VERIFY_NONE)
+# OpenSSL::SSL.send(:remove_const, :VERIFY_PEER)
+# OpenSSL::SSL.const_set(:VERIFY_PEER, OpenSSL::SSL::VERIFY_NONE)
