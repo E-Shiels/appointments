cc0f3044c3d648f19fc3417a656663b35362cd60
Eric Shiels <ericshiels2@gmail.com>
2019-06-21 13:36:54 -0400

    Prettify

--- app/assets/javascripts/appointments.js
+++ app/assets/javascripts/appointments.js
@@ -12,18 +12,22 @@ function Appointment(obj) {
 }
 Appointment.prototype.patientUrl = function() {
   return `/patients/${this.patientSlug}`;
-}
+};
 Appointment.prototype.returnHTML = function() {
   return `<div>
               <table>
                 <tr><th>Date</th><td>${this.date}</td></tr>
                 <tr><th>Time</th><td>${this.time}</td></tr>
-                <tr><th>Patient</th><td><a href='${this.patientUrl()}'>${this.patientName}</a></td></tr>
+                <tr><th>Patient</th><td><a href='${this.patientUrl()}'>${
+    this.patientName
+  }</a></td></tr>
               </table>
-              <a href='/appointments/${this.id}' class='show-page-button'>View Full Details</a>
+              <a href='/appointments/${
+                this.id
+              }' class='show-page-button'>View Full Details</a>
               <hr>
-            </div>`
-}
+            </div>`;
+};
 
 //Invokes function and sets inverval to run function again
 function setIntervalImmediately(func, interval) {
@@ -32,31 +36,34 @@ function setIntervalImmediately(func, interval) {
 }
 
 function getAndAppendAppointments() {
-  $.getJSON('/appointments').success(function(data) {
-    let i;
-    let appointments = [];
-    for (i = 0; i < data.length; i++) {
-      let a = new Appointment(
-        data[i]);
-      appointments.push(a);
-    }
-    //Empty appointments section and add appointments
-    $('#doctor-appointments-section').empty();
-    appointments.forEach(function(a) {
-      $('#doctor-appointments-section').append(a.returnHTML());
+  $.getJSON("/appointments")
+    .success(function(data) {
+      let i;
+      let appointments = [];
+      for (i = 0; i < data.length; i++) {
+        let a = new Appointment(data[i]);
+        appointments.push(a);
+      }
+      //Empty appointments section and add appointments
+      $("#doctor-appointments-section").empty();
+      appointments.forEach(function(a) {
+        $("#doctor-appointments-section").append(a.returnHTML());
+      });
+    })
+    .fail(function(jqXHR, textStatus, errorThrown) {
+      const errorMessage =
+        "getJSON request failed! " + textStatus + " - " + errorThrown;
+      alert(errorMessage);
     });
-  }).fail(function(jqXHR, textStatus, errorThrown) {
-    const errorMessage = 'getJSON request failed! ' + textStatus + ' - ' + errorThrown;
-    alert(errorMessage);
-  });
 }
 
 $(function() {
-  $('#quick-create-form').on("submit", function(e) {
+  $("#quick-create-form").on("submit", function(e) {
     let url = this.action;
     let formData = $(this).serialize();
-    $.post(url, formData, function() {}).success($('#quick-create-form')[0].reset())
-      .then($('#appointments-state-header').html("Today's Appointments"))
+    $.post(url, formData, function() {})
+      .success($("#quick-create-form")[0].reset())
+      .then($("#appointments-state-header").html("Today's Appointments"))
       .then(getAndAppendAppointments());
     e.preventDefault();
   });
